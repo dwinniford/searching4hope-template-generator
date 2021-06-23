9b705d065f34bacf576c17316b2d08c6a6e0651f
David Winniford <dwinniford@gmail.com>
2021-06-23 18:50:46 -0500

    demo day files

--- render.js
+++ render.js
@@ -1,13 +1,14 @@
 let ejs = require('ejs')
 let fs = require('fs')
-let data = require('./test-data/data.json')
+// let data = require('./test-data/data.json')
+let data = require('./test-data/howtobuyheroin.json')
 
 ejs.renderFile('./templates/index.ejs', data, {}, function(err, str) {
     if(err) {
         console.log(err)
     } else {
         // console.log(str)
-        fs.writeFile('test_output.html', str, function (err) {
+        fs.writeFile('index.html', str, function (err) {
             if (err) return console.log(err);
         });
     }
